c4a76bbeeff4226c96f4d5ce1dbb052c3930eed1
unknown <adriellekim23@gmail.com>
2024-03-15 11:58:48 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import Navbar from "./components/Navbar";
 import YourRecipes from "./components/YourRecipes";
 
-import img2 from "./img/img2.jpeg";
-
 function App() {
   return (
     <div className="App">
       <Navbar />
-
-      <header className="App-header">
-       {/* <img src={img2} className="header_img" alt="header_img" /> */}
-        
-      </header>
 
       <main>
         <YourRecipes />
